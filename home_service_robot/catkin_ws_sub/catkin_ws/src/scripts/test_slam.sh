46588483d134f0c4e71e6c16438f7548dd905cd4
anssun <anshulsungra@gmail.com>
2023-07-08 21:13:58 -0400

    adding files

---
+++ home_service_robot/catkin_ws_sub/catkin_ws/src/scripts/test_slam.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+export TURTLEBOT_GAZEBO_WORLD_FILE=$(rospack find turtlebot_gazebo)/worlds/Anshul_Sungra.world
+export TURTLEBOT_GAZEBO_MAP_FILE=$(rospack find turtlebot_gazebo)/maps/Anshul_Sungra.yaml
+
+xterm  -e  "roslaunch turtlebot_gazebo turtlebot_world.launch" &
+sleep 5
+xterm  -e  "roslaunch turtlebot_gazebo gmapping_demo.launch" &
+sleep 5
+xterm  -e  "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+xterm  -e  "roslaunch turtlebot_teleop keyboard_teleop.launch"
+
+
+
